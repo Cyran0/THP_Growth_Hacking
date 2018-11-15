28e331ccbbde1e0abe3ab88156af83a2c4ddff7a
Cyrano <bourgouinevan@gmail.com>
2018-11-15 23:02:45 +0100

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,12 +11,6 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery
-//= require jquery_ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-//= require jquery/jquery.min
-//= require bootstrap/js/bootstrap.bundle.min
-//= require jquery-easing/jquery.easing.min
-//= require js/stylish-portfolio.min.js
